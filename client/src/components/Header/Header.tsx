61877667de02bd6437b814de9b292af750c747f8
Alban <abujupaj@gmail.com>
2022-03-03 18:12:12 +0100

    Code Cleanup & Project base stylesheets setup. WIP: Navigation

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Logo from "../../assets/logo.svg";
+
+import "./Header.scss";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={Logo} alt="Cosmos" />
+      </div>
+
+      <nav className="header__navigation">
+        <ul>
+          <li>Market</li>
+          <li>Ecosystem</li>
+          <li>Community</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+
+      <div className="header__wallet">
+          <button className="button button--default">Connect Wallet</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
