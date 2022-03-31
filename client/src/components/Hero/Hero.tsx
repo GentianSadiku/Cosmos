6e3495e38582e63a8ff0a20002ebb2715daaace1
Alban <abujupaj@gmail.com>
2022-03-31 03:57:39 +0200

    Created AppContext to manage AppState for UI i.e Modals

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { Types } from "../../context/reducers";
+
 import GradientLine from "../GradientLine/GradientLine";
 import "./Hero.scss";
 
-const Hero: React.FC = () => {
+function Hero(): JSX.Element {
+  const { state, dispatch } = useContext(AppContext);
+
   return (
-    <section className="hero">
+    <section
+      className="hero"
+      style={{ opacity: state.connectWalletModal ? 0 : 1 }}
+    >
       <h1>
         SEND CRYPTO <br /> ACCROS THE WORLD
       </h1>
       <div className="description">
-        <GradientLine className="gradient-line--static-width"/>
-        <p>
+        <GradientLine className="gradient-line--static-width" />
+        <p
+          onClick={() => {
+            dispatch({
+              type: Types.connectWalletModalOpen,
+            });
+          }}
+        >
           Swap, earn, and build on the leading decentralized crypto trading
           protocol.
         </p>
-        <GradientLine className="gradient-line--static-width"/>
+        <GradientLine className="gradient-line--static-width" />
       </div>
     </section>
   );
-};
+}
 
 export default Hero;
